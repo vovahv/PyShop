b877f9099fbab015102e1feb6389489755bc085f
Vova <vova.hvozdetskyi@gmail.com>
2025-09-10 20:53:00 -0400

    add offer model

--- products/models.py
+++ products/models.py
@@ -6,3 +6,8 @@ class Product(models.Model):
     price = models.FloatField()
     stock = models.IntegerField()
     image_url = models.CharField(max_length=2083)
+
+class Offer(models.Model):
+    code = models.CharField(max_length=10)
+    description = models.TextField(max_length=255)
+    discount = models.FloatField()
