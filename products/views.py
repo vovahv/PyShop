7ae6e75424747d451071e61e09fe8b1e4c85a646
Vova <vova.hvozdetskyi@gmail.com>
2025-09-10 20:33:02 -0400

    create Products model and first migration

--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Index.")
+
+def new(request):
+    return HttpResponse("New Products")
